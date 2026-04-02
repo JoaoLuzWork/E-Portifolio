6e7fc6d66adcbe119c11f085b21c158455da8906
João Pedro Luz <101302893+JoaoLuzWork@users.noreply.github.com>
2026-04-02 12:48:36 +0100

    some changes and atualizations

---
+++ script.js
@@ -0,0 +1,24 @@
+
+window.addEventListener("load", function(){
+header.innerHTML = `
+    <div class="navBar">
+        <div>
+            <H1>Joao Pedro Luz Rodrigues</H1>
+        </div>
+        <div class="navItems">
+            <a href="index.html">Home</a> 
+            <a href="about.html">About</a> 
+            <a href="hobbies.html">Hobbies</a> 
+            
+            <a href="contact.html">Contact</a> 
+        </div>
+    </div>`;
+
+    footer.innerHTML = `
+    <div class="socialMedias">
+        <a href="https://www.instagram.com/jp_rodriguesluz/"><img class="instaLogo" src="./img/big-instagram-logo-svgrepo-com.svg" alt=""></a>
+        <a href=""><img class="linkedinLogo" src="./img/linkedin-svgrepo-com.svg" alt=""></a>
+        <a href=""><img class="emailLogo" src="./img/email-chat-svgrepo-com.svg" alt=""></a>
+    </div>`;
+});
+
