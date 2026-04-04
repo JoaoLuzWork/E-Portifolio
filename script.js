61cf0f0d62dc9f945707f4fdaca32a467ff9a059
João Pedro Luz <101302893+JoaoLuzWork@users.noreply.github.com>
2026-04-04 01:32:05 +0100

    whole change in the pages

--- script.js
+++ script.js
@@ -8,17 +8,20 @@ header.innerHTML = `
         <div class="navItems">
             <a href="index.html">Home</a> 
             <a href="about.html">About</a> 
+            <a href="Qualifications.html">Qualifications</a> 
             <a href="hobbies.html">Hobbies</a> 
-            
-            <a href="contact.html">Contact</a> 
         </div>
     </div>`;
 
     footer.innerHTML = `
     <div class="socialMedias">
-        <a href="https://www.instagram.com/jp_rodriguesluz/"><img class="instaLogo" src="./img/big-instagram-logo-svgrepo-com.svg" alt=""></a>
+        <a href="https://www.instagram.com/jp_rodriguesluz/"><img class="instaLogo" src="./img/instaLogo.svg" alt=""></a>
         <a href=""><img class="linkedinLogo" src="./img/linkedin-svgrepo-com.svg" alt=""></a>
-        <a href=""><img class="emailLogo" src="./img/email-chat-svgrepo-com.svg" alt=""></a>
-    </div>`;
+        <a href=""><img class="emailLogo" src="./img/emailLogo.svg" alt=""></a>
+    </div>
+    <div>
+        <P>Joao Pedro Luz Rodrigues 2026</P>
+    </div>
+    `;
 });
 
