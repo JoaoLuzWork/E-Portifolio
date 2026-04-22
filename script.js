e83509398adfe87bd8cc421519576368a84ac8b9
João Pedro Luz <101302893+JoaoLuzWork@users.noreply.github.com>
2026-04-22 09:18:42 +0100

    testing somethings

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 
 window.addEventListener("load", function(){
-header.innerHTML = `
-    <div class="navBar">
-        <div>
-            <H1>Joao Pedro Luz Rodrigues</H1>
-        </div>
-        <div class="navItems">
-            <a href="index.html">Home</a> 
-            <a href="about.html">About</a> 
-            <a href="qualifications.html">Qualifications</a> 
-            <a href="hobbies.html">Hobbies</a> 
-        </div>
-    </div>`;
+    header.innerHTML = `
+        <div class="navBar">
+            <div>
+                <H1>Joao Pedro Luz Rodrigues</H1>
+            </div>
+            <div class="navItems">
+                <a href="index.html">Home</a> 
+                <a href="about.html">About</a> 
+                <a href="qualifications.html">Qualifications</a> 
+                <a href="hobbies.html">Hobbies</a> 
+            </div>
+        </div>`;
 
     footer.innerHTML = `
-    <div class="socialMedias">
-        <a href="https://www.instagram.com/jp_rodriguesluz/"><img class="instaLogo" src="./img/instaLogo.svg" alt=""></a>
-        <a href=""><img class="linkedinLogo" src="./img/linkedin-svgrepo-com.svg" alt=""></a>
-        <a href=""><img class="emailLogo" src="./img/emailLogo.svg" alt=""></a>
-    </div>
-    <div>
-        <P>Joao Pedro Luz Rodrigues 2026</P>
-    </div>
-    `;
+        <div class="socialMedias">
+            <a href="https://www.instagram.com/jp_rodriguesluz/"><img class="instaLogo" src="./img/instaLogo.svg" alt=""></a>
+            <a href=""><img class="linkedinLogo" src="./img/linkedin-svgrepo-com.svg" alt=""></a>
+            <a href=""><img class="emailLogo" src="./img/emailLogo.svg" alt=""></a>
+        </div>
+        <div>
+            <P>Joao Pedro Luz Rodrigues 2026</P>
+        </div>
+        `;
 });
 
